32936a221a04888ba99d881c00127bf7ea992556
Masterchef365 <duncan.freeman1@gmail.com>
2015-12-21 16:56:30 -0800

    First Commit
    Hooray!

---
+++ UI.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Plugin_Modder_UI
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RefreshList = new System.Windows.Forms.Button();
+            this.OpenPluginFolder = new System.Windows.Forms.Button();
+            this.activePluginsBox = new System.Windows.Forms.CheckedListBox();
+            this.RunSE = new System.Windows.Forms.Button();
+            this.BinLocationTextBox = new System.Windows.Forms.TextBox();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.RefreshList);
+            this.panel1.Controls.Add(this.OpenPluginFolder);
+            this.panel1.Controls.Add(this.activePluginsBox);
+            this.panel1.Controls.Add(this.RunSE);
+            this.panel1.Controls.Add(this.BinLocationTextBox);
+            this.panel1.Controls.Add(this.InstallButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(478, 338);
+            this.panel1.TabIndex = 0;
+            // 
+            // RefreshList
+            // 
+            this.RefreshList.Location = new System.Drawing.Point(164, 310);
+            this.RefreshList.Name = "RefreshList";
+            this.RefreshList.Size = new System.Drawing.Size(167, 23);
+            this.RefreshList.TabIndex = 5;
+            this.RefreshList.Text = "Refresh List";
+            this.RefreshList.UseVisualStyleBackColor = true;
+            this.RefreshList.Click += new System.EventHandler(this.RefreshList_Click);
+            // 
+            // OpenPluginFolder
+            // 
+            this.OpenPluginFolder.Location = new System.Drawing.Point(4, 310);
+            this.OpenPluginFolder.Name = "OpenPluginFolder";
+            this.OpenPluginFolder.Size = new System.Drawing.Size(153, 23);
+            this.OpenPluginFolder.TabIndex = 4;
+            this.OpenPluginFolder.Text = "Open In Explorer";
+            this.OpenPluginFolder.UseVisualStyleBackColor = true;
+            this.OpenPluginFolder.Click += new System.EventHandler(this.OpenPluginFolder_Click);
+            // 
+            // activePluginsBox
+            // 
+            this.activePluginsBox.FormattingEnabled = true;
+            this.activePluginsBox.Location = new System.Drawing.Point(4, 59);
+            this.activePluginsBox.Name = "activePluginsBox";
+            this.activePluginsBox.Size = new System.Drawing.Size(471, 244);
+            this.activePluginsBox.TabIndex = 3;
+            this.activePluginsBox.SelectedIndexChanged += new System.EventHandler(this.activePluginsBox_SelectedIndexChanged);
+            // 
+            // RunSE
+            // 
+            this.RunSE.Location = new System.Drawing.Point(163, 30);
+            this.RunSE.Name = "RunSE";
+            this.RunSE.Size = new System.Drawing.Size(168, 23);
+            this.RunSE.TabIndex = 2;
+            this.RunSE.Text = "Run SpaceEngineers";
+            this.RunSE.UseVisualStyleBackColor = true;
+            this.RunSE.Click += new System.EventHandler(this.RunSE_Click);
+            // 
+            // BinLocationTextBox
+            // 
+            this.BinLocationTextBox.Location = new System.Drawing.Point(4, 4);
+            this.BinLocationTextBox.Name = "BinLocationTextBox";
+            this.BinLocationTextBox.Size = new System.Drawing.Size(471, 20);
+            this.BinLocationTextBox.TabIndex = 1;
+            this.BinLocationTextBox.Text = "Enter in SE Bin64 Here";
+            this.BinLocationTextBox.TextChanged += new System.EventHandler(this.BinLocationTextBox_TextChanged);
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(4, 30);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(153, 23);
+            this.InstallButton.TabIndex = 0;
+            this.InstallButton.Text = "Install SEMultiPlugin";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(502, 362);
+            this.Controls.Add(this.panel1);
+            this.Name = "UI";
+            this.Text = "Plugin Modder UI";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox BinLocationTextBox;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button OpenPluginFolder;
+        private System.Windows.Forms.CheckedListBox activePluginsBox;
+        private System.Windows.Forms.Button RunSE;
+        private System.Windows.Forms.Button RefreshList;
+    }
+}
+
---
+++ UI.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Plugin_Modder_UI
+{
+    public partial class UI : Form
+    {
+        string bin64Location = "";
+        string pluginsLocation = "";
+        string candidatesLocation = "";
+
+        public string[] pluginCandidates = { };
+        public UI()
+        {
+            InitializeComponent();
+            activePluginsBox.CheckOnClick = true; //Just one click thanks
+            
+            if (Directory.Exists(@"C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\"))
+            {
+                bin64Location = @"C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\";
+                BinLocationTextBox.Text = bin64Location;
+            }
+            
+            pluginsLocation = bin64Location + @"PluginManager\Plugins\";
+            candidatesLocation = bin64Location + @"PluginManager\PluginCandidates\";
+            
+            if (Directory.Exists(pluginsLocation) && Directory.Exists(candidatesLocation) && File.Exists(bin64Location + "SEMultiPlugin.dll"))
+            {
+                ReloadCheckboxes(candidatesLocation, pluginsLocation);
+                InstallButton.Text = "Uninstall SEPluginManager";
+                InstallButton.Enabled = true;
+                RunSE.Enabled = true;
+                RefreshList.Enabled = true;
+                OpenPluginFolder.Enabled = true;
+            } else
+            {
+                InstallButton.Text = "Install SEPluginManager";
+                RunSE.Enabled = false;
+                RefreshList.Enabled = false;
+                OpenPluginFolder.Enabled = false;
+            }
+
+        }
+
+        private void ReloadCheckboxes(string candidates, string alreadyLoaded)
+        {
+            if (Directory.Exists(candidates) && Directory.Exists(alreadyLoaded))
+            {
+                activePluginsBox.Items.Clear();
+                foreach (string current in Directory.GetFiles(candidates))
+                {
+                    int index = activePluginsBox.Items.Add(Path.GetFileName(current));
+                    bool hasIt = false;
+                    foreach (string plugin in Directory.GetFiles(alreadyLoaded))
+                    {
+                        if (Path.GetFileName(plugin) == Path.GetFileName(current)) { hasIt = true; break; }
+                    }
+                    if (hasIt)
+                    {
+                        activePluginsBox.SetItemChecked(index, true);
+                    }
+                }
+                activePluginsBox.Refresh();
+            } 
+        }
+
+        private void BinLocationTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string text = (sender as TextBox).Text;
+            if (Directory.Exists(text))
+            {
+                bin64Location = text;
+                if (Directory.Exists(bin64Location + @"PluginManager\PluginCandidates\") && Directory.Exists(bin64Location + @"PluginManager\Plugins\") && File.Exists(bin64Location + "SEMultiPlugin.dll"))
+                {
+                    pluginsLocation = bin64Location + @"PluginManager\Plugins\";
+                    candidatesLocation = bin64Location + @"PluginManager\PluginCandidates\";
+                    ReloadCheckboxes(candidatesLocation, pluginsLocation);
+                    InstallButton.Text = "Uninstall SEPluginManager";
+                    InstallButton.Enabled = true;
+                    RunSE.Enabled = true;
+                    RefreshList.Enabled = true;
+                    OpenPluginFolder.Enabled = true;
+                } else
+                {
+                    InstallButton.Text = "Install SEPluginManager";
+                    InstallButton.Enabled = true;
+                    RunSE.Enabled = false;
+                    RefreshList.Enabled = false;
+                    OpenPluginFolder.Enabled = false;
+                }
+            } else
+            {
+                InstallButton.Enabled = false;
+                RunSE.Enabled = false;
+                RefreshList.Enabled = false;
+                OpenPluginFolder.Enabled = false;
+            }
+        }
+
+        private void OpenPluginFolder_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(pluginsLocation) && Directory.Exists(candidatesLocation))
+            {
+                Process.Start("explorer.exe", candidatesLocation);
+            }
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(pluginsLocation) && Directory.Exists(candidatesLocation) && File.Exists(bin64Location + "SEMultiPlugin.dll"))
+            { //Uninstall
+                foreach (string file in Directory.GetFiles(pluginsLocation))
+                {
+                    File.Delete(file);
+                }
+                Directory.Delete(pluginsLocation);
+                File.Delete(bin64Location + "SEMultiPlugin.dll");
+                RunSE.Enabled = false;
+                RefreshList.Enabled = false;
+                OpenPluginFolder.Enabled = false;
+                InstallButton.Text = "Install SEPluginManager";
+                activePluginsBox.Items.Clear();
+            }
+            else
+            {
+                if (Directory.Exists(bin64Location))
+                {
+                    Directory.CreateDirectory(bin64Location + @"PluginManager\Plugins\");
+                    Directory.CreateDirectory(bin64Location + @"PluginManager\PluginCandidates\");
+                    pluginsLocation = bin64Location + @"PluginManager\Plugins\";
+                    candidatesLocation = bin64Location + @"PluginManager\PluginCandidates\";
+                }
+
+                string dllSource = Directory.GetCurrentDirectory() + @"\SEMultiPlugin.dll";
+                if (File.Exists(dllSource))
+                {
+                    File.Copy(dllSource, bin64Location + Path.GetFileName(dllSource));
+                }
+                ReloadCheckboxes(candidatesLocation, pluginsLocation);
+                Process.Start("explorer.exe", candidatesLocation);
+                InstallButton.Text = "Uninstall SEPluginManager";
+                InstallButton.Enabled = true;
+                RunSE.Enabled = true;
+                RefreshList.Enabled = true;
+                OpenPluginFolder.Enabled = true;
+            }
+        }
+
+        private void RunSE_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(bin64Location + "SpaceEngineers.exe"))
+            {
+                Process.Start(bin64Location + "SpaceEngineers.exe", "-plugin " + "SEMultiPlugin.dll");
+            }
+        }
+
+        private void activePluginsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> confirmed = new List<string>();
+            foreach (object item in activePluginsBox.CheckedItems)
+            {
+                confirmed.Add(activePluginsBox.GetItemText(item));
+            }
+
+            //Delete all
+            foreach (string current in Directory.GetFiles(pluginsLocation))
+            {
+                File.Delete(current);
+            }
+
+            //Add them
+            foreach (string current in Directory.GetFiles(candidatesLocation))
+            {
+                if (confirmed.Contains(Path.GetFileName(current)))
+                {
+                    File.Copy(current, pluginsLocation + Path.GetFileName(current));
+                }
+            }
+
+
+        }
+
+        private void RefreshList_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(pluginsLocation) && Directory.Exists(candidatesLocation))
+            {
+                ReloadCheckboxes(candidatesLocation, pluginsLocation);
+            }
+        }
+    }
+}
